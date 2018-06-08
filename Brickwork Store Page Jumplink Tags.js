bad257badb71d8b60a570d4b0f9a6a6ad8db1a2f
Rios <jrios@c21stores.com>
2018-06-08 10:25:20 -0400

    Brickwork Store Page Jumplink Tags
    located in Locations Store Default updated

--- Brickwork Store Page Jumplink Tags.js
+++ Brickwork Store Page Jumplink Tags.js
@@ -1,1 +1,11 @@
-Brickwork Store Page Jumplink Tags
+window.addEventListener("DOMContentLoaded", function() {
+  $('.bwt-store-nav-list a[href="#appointments"]').click(function() {
+    cmCreateElementTag("Services and Appointments - " + $(".bwt-store-show").attr("data-bw-track-event-value"), "Services and Appointments");
+  });
+  $('.bwt-store-nav-list a[href="#events"]').click(function() {
+    cmCreateElementTag("Events - " + $(".bwt-store-show").attr("data-bw-track-event-value"), "Store Events");
+  });
+  $('.bwt-store-nav-list a[href="#stylists"]').click(function() {
+    cmCreateElementTag("Associates - " + $(".bwt-store-show").attr("data-bw-track-event-value"), "Associates");
+  });
+});
