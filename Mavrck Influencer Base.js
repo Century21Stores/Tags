5914539ed4e21709974ad21a1ecbb324ada01059
Rios <jrios@c21stores.com>
2018-08-02 11:49:52 -0400

    TAGS: Mavrck Influencer Base

---
+++ Mavrck Influencer Base.js
@@ -0,0 +1,8 @@
+(function() {
+  window.mvk = window.mvk || function(){(mvk.q=mvk.q||[]).push(arguments)};
+  mvk('config', {id:'c21stores'});
+  var n = document.createElement('script');
+  n.src = document.location.protocol + '//plugin.mavrck.co/mavrck_plugin.js', n.async = !0;
+  var c = document.getElementsByTagName('script')[0];
+  c.parentNode.insertBefore(n, c);
+})();
