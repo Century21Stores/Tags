50ad16b3447deea78540839f3c56e6fc30cdc01c
Rios <jrios@c21stores.com>
2018-08-02 11:31:52 -0400

    TAGS: LiveChat Tracking - rails global DEAFULT & LOcation PAges DEFAULT

---
+++ LiveChat Tracking.js
@@ -0,0 +1,10 @@
+(function() {
+  // if (digitalData.pageInstanceID.endsWith('/checkout/payment') || digitalData.pageInstanceID.endsWith('/instant_credit/apply') || digitalData.pageInstanceID.endsWith('/checkout/apply')) {
+    // return;
+  // }
+  window.__lc = window.__lc || {};
+  window.__lc.license = 8855584;
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
