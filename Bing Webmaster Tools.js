1f3d029950246365c7840a35f1ef13b2037e3ebe
Rios <jrios@ny-sixthcs-02.c21stores.com>
2018-09-20 10:28:30 -0400

    TAGS : Bing Webmaster Tools
    First Push of Bing Webmaster Tools tag

---
+++ Bing Webmaster Tools.js
@@ -0,0 +1,7 @@
+// 09.20.18 - JMRV
+// Original Tag
+// <meta name="msvalidate.01" content="160CC63731D9CD90D46ED9311E8E18D4" />
+var meta = document.createElement('meta');
+meta.name = "msvalidate.01";
+meta.content = "160CC63731D9CD90D46ED9311E8E18D4";
+document.getElementsByTagName('head')[0].appendChild(meta);
