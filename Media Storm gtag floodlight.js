9f31075b210a7cc20232b7acb5c7c9328a0eb109
Rios <jrios@c21stores.com>
2019-02-15 08:49:07 -0500

    floodlight

---
+++ Media Storm gtag floodlight.js
@@ -0,0 +1,23 @@
+// <!-- Global site tag (gtag.js) - Google Marketing Platform -->
+!function() {
+  var n = document.createElement('script');
+  n.src = document.location.protocol + '//www.googletagmanager.com/gtag/js?id=DC-8908005', n.async = !0;
+  var c = document.getElementsByTagName('script')[0];
+  c.parentNode.insertBefore(n, c);
+}();
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date()); 
+  gtag('config', 'DC-8908005');
+ // End of global snippet: Please do not remove 
+
+
+// Event snippet for Century 21 universal tag on https://www.c21stores.com: Please do not remove.
+// Place this snippet on pages with events you’re tracking. 
+// Creation date: 09/11/2018
+  gtag('event', 'conversion', {
+    'allow_custom_scripts': true,
+    'send_to': 'DC-8908005/c21290/centu0+standard'
+  });
+ // End of event snippet: Please do not remove
